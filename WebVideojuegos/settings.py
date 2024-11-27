323c938b29d6bac16b8af8a4c260a201139783d0
DanielEstebanT <daniel.e.tobar.c@gmail.com>
2024-11-27 01:24:32 -0500

    postgres en docker

--- WebVideojuegos/settings.py
+++ WebVideojuegos/settings.py
@@ -75,12 +75,17 @@ WSGI_APPLICATION = 'WebVideojuegos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "db", # set in docker-compose.yml
+        "PORT": 5432, # default postgres port
+        }
         
     }
-}
+
 
 
 # Password validation
